8861c360e2405d47ff981e74651fde3a5d0b2fac
Chrisdd3365 <32330136+Chrisdd3365@users.noreply.github.com>
2018-10-02 15:55:08 +0200

    new commit
    implementing tabbar controller, navigation controller
    
    installing and importing alamofire

---
+++ Reciplease/Model/SearchService.swift
@@ -0,0 +1,10 @@
+//
+//  SearchService.swift
+//  Reciplease
+//
+//  Created by Christophe DURAND on 02/10/2018.
+//  Copyright © 2018 Christophe DURAND. All rights reserved.
+//
+
+import Foundation
+import Alamofire
